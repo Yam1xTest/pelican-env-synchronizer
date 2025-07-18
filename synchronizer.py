520eb3ce9db7878599f5034421db78e0f41964cd
Maxim Rychkov <81160491+Yam1x@users.noreply.github.com>
2025-07-18 08:41:36 +0500

    Update synchronizer.py

--- synchronizer.py
+++ synchronizer.py
@@ -6,6 +6,8 @@ if (os.getenv("SYNCHRONIZER_ENABLED") == "True"):
     # with check = True subprocess.run will throw error and pod will be completed with error
     subprocess.run(['git', 'clone', f'https://github.com/{os.getenv("ENV_REPOSITORY")}.git'], check = True)
 
+    subprocess.run(['env'], check = True)
+
     subprocess.run(['helmfile', 'cache', 'cleanup'], check = True)
 
     subprocess.run(['helmfile', '--environment', f'{os.getenv("NAMESPACE")}', '--namespace', f'{os.getenv("NAMESPACE")}', '-f', f'{os.getenv("PATH_TO_HELMFILE")}', 'apply'], check = True)
